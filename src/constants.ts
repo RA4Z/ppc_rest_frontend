1ad99786da0aed01971462210bf5ddf6aee72a51
RA4Z <rraz639@gmail.com>
2024-06-28 13:47:09 -0300

    Removendo proxyUrl

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,17 @@
 const prd = {
     env: "prd",
-    proxyUrl: "",
     url: "http://10.1.43.63:5000",
     cookieUrl: '__Secure-next-auth.session-token'
   };
   
   const dev = {
     env: "dev",
-    proxyUrl: "",
     url: "http://10.1.43.63:5000",
     cookieUrl: '__Secure-next-auth.session-token'
   };
   
   const local = {
     env: "local",
-    proxyUrl: "",
     url: "http://10.1.43.63:5000",
     cookieUrl: 'next-auth.session-token'
   };
