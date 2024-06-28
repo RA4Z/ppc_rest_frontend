0309c47a319a8c14730e83dc387483bb3597b42a
RA4Z <rraz639@gmail.com>
2024-06-28 15:44:55 -0300

    Criando componente de side bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,18 @@ import { Divider } from 'antd';
 import styles from './Header.module.scss'
 import Image from "next/image";
 import Link from 'next/link';
+import SideBar from 'components/SideBar';
 
 export default function Header() {
     return (
-        <>
+        <header className={styles.head}>
             <Link href={'/'}>
-                <header className={styles.center}>
+                <div className={styles.center}>
                     <h2>PPC Rest API Frontend APP</h2>
                     <Image className={styles.logo} src="/WEG.svg" alt="Logotipo da WEG" width={180} height={37} priority />
-                </header>
+                </div>
             </Link>
             <Divider />
-        </>
+        </header>
     )
 }
